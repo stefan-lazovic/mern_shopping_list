abef15d8e3c582d36909041f8ea592820ee9e80b
Stefan Lazovic <agios.stefanos@hotmail.com>
2020-02-04 19:57:41 +0100

    Deploying to Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const items = require('./routes/api/items')
+const path = require('path')
 
 const app = express()
 
@@ -17,6 +18,15 @@ mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
 
 // Use Routes
 app.use('/api/items', items)
+
+// Serve static assets (build folder) if we are in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const port = process.env.PORT || 5000
 
