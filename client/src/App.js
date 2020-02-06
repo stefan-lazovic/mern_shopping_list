a2076eb2141ce25cfe254b1e37d5679b6b345d43
Stefan Lazovic <agios.stefanos@hotmail.com>
2020-02-06 17:24:47 +0100

    added auth reducer to handle storing authentication state, JWT token and the user data

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,29 @@ import ItemModal from './components/ItemModal';
 import { Container } from 'reactstrap';
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/authActions';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <Provider store={store}>
-      <div className="App">
-        <AppNavbar />
-        <Container>
-          <ItemModal />
-          <ShoppingList />
-        </Container>
-      </div>
-    </Provider>
-  );
+class App extends React.Component {
+
+  componentDidMount() {
+    store.dispatch(loadUser())
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
